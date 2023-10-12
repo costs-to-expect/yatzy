cbf7af015522b2a11487c9dcadc2cec3acf354f6
Dean Blackborough <dean@g3d-development.com>
2023-10-12 22:34:13 +0100

    Release
    - Updated the changelog
    - Set the release date and version

--- config/app/config.php
+++ config/app/config.php
@@ -11,6 +11,6 @@ return [
     'error_email' => env('ERROR_EMAIL'),
     'cookie_user' => env('SESSION_NAME_USER'),
     'cookie_bearer' => env('SESSION_NAME_BEARER'),
-    'version' => '1.03.0',
-    'release_date' => '2nd July 2023'
+    'version' => '1.04.0',
+    'release_date' => '12th October 2023'
 ];
